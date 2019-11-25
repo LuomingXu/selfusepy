8fd432fd4da59a3004a39d84d6a196a46aa17ef2
LuomingXu <xjy46566696@gmail.com>
2019-11-25 17:26:02 +0800

    dev:     json deserialize

--- test/test.py
+++ test/test.py
@@ -1,23 +1,78 @@
-# encoding:utf-8
-
 import json
-from param import Data, Body, Json
-from url import Request
-from utils import Logger
-from urllib3.response import HTTPResponse
 
-# if __name__ == '__main__':
-#   p = Data('param')
-#   b = Body('one', 'two')
-#   request = Request()
-#   log = Logger().logger
-#
-#   url = 'http://localhost:8080/test'
-#   res: HTTPResponse = request.put(url, b, **p.__dict__)
-#   log.info(res.data)
+
+class Demo(object):
+  def __init__(self):
+    self.y: int = -1
+
+
+class Point(object):
+
+  def unserialize_object(d: dict):
+    for key, value in d.items():
+      cls = type(getattr(Point(), key))
+      obj = cls.__new__(cls)
+      setattr(obj, key, value)
+    return obj
+
+  def __demo__(self):
+    for item in vars(self):
+      cls = type(getattr(self, item))
+      try:
+        print('item: %s, class: %s' % (item, cls))
+        obj = eval(cls())
+        print('')
+        print(vars(obj))
+      except BaseException as e:
+        pass
+
+  def __init__(self):
+    self.x: int = -1
+    self.demo: Demo = Demo()
+
+    self.__demo__()
+
+
+classes = {
+  'Point': Point,
+  'Demo': Demo
+}
+
+
+def serialize_instance(obj):
+  d = {'__classname__': type(obj).__name__}
+  d.update(vars(obj))
+  return d
+
+
+def unserialize_object(d):
+  clsname = d.pop('__classname__', None)
+  if clsname:
+    cls = classes[clsname]
+    obj = cls.__new__(cls)  # Make instance without calling __init__
+    for key, value in d.items():
+      setattr(obj, key, value)
+    return obj
+  else:
+    return d
+
 
 if __name__ == '__main__':
-  f = open('test.json', 'r')
-  data = f.read()
-  clazz: Json = json.loads(data, object_hook = Json.from_dict)
-  print(clazz.body.two)
+  # p = Point(1, 2, Demo(3))
+  # s = json.dumps(p, default = serialize_instance)
+  # print(s)
+  # p = Point()
+  # cls = type(p.p)
+  # cls .__new__(cls)
+  # print(cls)
+  # print(type(getattr(p, 'p')))
+  print(vars(Point.__new__(Point)))
+  dskfj = Point()
+  # f = open('demo.json', 'r')
+  # s = f.read()
+  # p = json.loads(s, object_hook = unserialize_object)
+  #
+  # f = open('test.json', 'r')
+  # s = f.read()
+  # p1 = json.loads(s, object_hook = Point.unserialize_object)
+  # print(p1)
